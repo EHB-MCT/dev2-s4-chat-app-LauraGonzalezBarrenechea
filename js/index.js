f20822d6092fa009a730134e51493d3ad91a9578
Laura Gonzalez <laura.gonzalez.barrenechea@student.ehb.be>
2023-03-15 15:39:54 +0100

    Commit - 1

--- js/index.js
+++ js/index.js
@@ -7,10 +7,18 @@ const chat = {
     sendMessage() {
     },
     fetchMessages() {
+        fetch ("https://dev2chat.onrender.com/messages")
+        .then (function (response) {
+            return response.json();
+        })
+        .then (function(messages) {
+            console.log (messages);
+            document.querySelector("messageContainer").innerHTML = messages(0).message;
+        });
     },
-    renderMessage(message) {
-    }
 
-}
+    renderMessage(message) {}
 
+
+}    
 chat.init();
